97676e1b1a58e1583ecc744152aeb17b38c73de1
JasonChanPy <2282727499@qq.com>
2024-10-13 23:50:18 +0800

    add app

--- server.py
+++ server.py
@@ -2,3 +2,15 @@
 # Date: 2024/10/13
 # Author: Jason
 # File: server
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def hello() -> str:
+    return "hi!"
+
+if __name__ == '__main__':
+    uvicorn.run(app=app)
